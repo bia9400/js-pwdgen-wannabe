4a34bcbe1b281ce69b5ab7df7eea30da5f2346db
bia9400 <ferro.biagio@gmail.com>
2022-05-17 16:37:33 +0200

    end

---
+++ app.js
@@ -0,0 +1,6 @@
+let nomeUtente=prompt("Qual è il tuo nome?");
+let cognomeUtente=prompt("Qual è il tuo cognome?");
+let coloreUtente=prompt("Qual è il tuo colore preferito");
+let containerhtml=document.getElementById("mah")
+message=(`${nomeUtente}${cognomeUtente}${coloreUtente}22`)
+mah.innerHTML="<h2>"+message+"</> ";
